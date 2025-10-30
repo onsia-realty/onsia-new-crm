3a0c58510ae07a491777c5ce8708f49f7a6a1253
Dae Gyeum <your-email@example.com>
2025-10-30 13:27:39 +0900

    fix: NextAuth Session 타입에 passwordResetRequired 추가

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,12 +4,14 @@ import NextAuth, { DefaultSession } from 'next-auth'
 declare module 'next-auth' {
   interface User {
     role: Role
+    passwordResetRequired?: boolean
   }
-  
+
   interface Session {
     user: {
       id: string
       role: Role
+      passwordResetRequired?: boolean
     } & DefaultSession['user']
   }
 }
